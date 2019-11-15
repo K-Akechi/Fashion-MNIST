2855c85bb396e2f88243b1a7f531d2b3667749a7
akechi <cyh763467593@gmail.com>
2019-11-15 21:31:51 +0800

    first commit

---
+++ model.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+
+
+def weight_variable(shape):
+    # initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.get_variable('weights', dtype='float32', shape=shape, initializer=tf.contrib.layers.xavier_initializer())
+
+
+def bias_variable(shape):
+    # initial = tf.constant(0.1, shape=shape, dtype=float)
+    return tf.get_variable('bias', shape=shape, dtype='float32', initializer=tf.constant_initializer(0.05))
+
+
+def conv2d(x, W, stride):
+    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def model2(image, keep_prob):
+    with tf.variable_scope('conv1'):
+        w_conv1 = weight_variable([5, 5, 1, 40])
+        b_conv1 = bias_variable([40])
+        h_conv1 = tf.nn.relu(conv2d(image, w_conv1, 1) + b_conv1)
+
+    with tf.variable_scope('maxpool1'):
+        h_pool1 = max_pool_2x2(h_conv1)
+
+    with tf.variable_scope('conv2'):
+        w_conv2 = weight_variable([5, 5, 40, 20])
+        b_conv2 = bias_variable([20])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2, 1) + b_conv2)
+
+    with tf.variable_scope('maxpool2'):
+        h_pool2 = max_pool_2x2(h_conv2)
+        flat = tf.reshape(h_pool2, [-1, 7 * 7 * 20])
+
+    with tf.variable_scope('fc1'):
+        w_fc1 = weight_variable([7 * 7 * 20, 1024])
+        b_fc1 = bias_variable([1024])
+        h_fc1 = tf.matmul(flat, w_fc1) + b_fc1
+        intermediate = h_fc1
+        h_fc1 = tf.nn.relu(h_fc1)
+        # h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    with tf.variable_scope('fc2'):
+        w_fc2 = weight_variable([1024, 10])
+        b_fc2 = bias_variable([10])
+        y = tf.matmul(h_fc1, w_fc2) + b_fc2
+
+    return y, intermediate
+
+
+def model3(image, keep_prob):
+    with tf.variable_scope('conv1'):
+        w_conv1 = weight_variable([5, 5, 1, 40])
+        b_conv1 = bias_variable([40])
+        h_conv1 = tf.nn.relu(conv2d(image, w_conv1, 1) + b_conv1)
+
+    with tf.variable_scope('maxpool1'):
+        h_pool1 = max_pool_2x2(h_conv1)
+
+    with tf.variable_scope('conv2'):
+        w_conv2 = weight_variable([5, 5, 40, 20])
+        b_conv2 = bias_variable([20])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2, 1) + b_conv2)
+
+    with tf.variable_scope('maxpool2'):
+        h_pool2 = max_pool_2x2(h_conv2)
+        flat = tf.reshape(h_pool2, [-1, 7 * 7 * 20])
+
+    with tf.variable_scope('fc1'):
+        w_fc1 = weight_variable([7 * 7 * 20, 320])
+        b_fc1 = bias_variable([320])
+        h_fc1 = tf.nn.relu(tf.matmul(flat, w_fc1) + b_fc1)
+        # h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    with tf.variable_scope('fc2'):
+        w_fc2 = weight_variable([320, 160])
+        b_fc2 = bias_variable([160])
+        h_fc2 = tf.nn.relu(tf.matmul(h_fc1, w_fc2) + b_fc2)
+
+    with tf.variable_scope('fc3'):
+        w_fc3 = weight_variable([160, 80])
+        b_fc3 = bias_variable([80])
+        h_fc3 = tf.matmul(h_fc2, w_fc3) + b_fc3
+        h_fc3 = tf.nn.relu(h_fc3)
+
+    with tf.variable_scope('fc4'):
+        w_fc4 = weight_variable([80, 40])
+        b_fc4 = bias_variable([40])
+        h_fc4 = tf.matmul(h_fc3, w_fc4) + b_fc4
+        intermediate = h_fc4
+        h_fc4 = tf.nn.relu(h_fc4)
+
+    with tf.variable_scope('fc5'):
+        w_fc5 = weight_variable([40, 10])
+        b_fc5 = bias_variable([10])
+        y = tf.matmul(h_fc4, w_fc5) + b_fc5
+
+    return y, intermediate
---
+++ train.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+import scipy.misc
+import os
+import time
+import model
+
+train_dir = './model3/'
+mnist = input_data.read_data_sets("data/fashion", one_hot=True)
+
+
+def main(argv=None):
+    x = tf.placeholder(tf.float32, [None, 784])
+    y_ = tf.placeholder(tf.float32, [None, 10])
+    image = tf.reshape(x, [-1, 28, 28, 1])
+    keep_prob = tf.placeholder(tf.float32)
+    y, _ = model.model3(image, keep_prob)
+    # print(y)
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_, logits=y))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    saver = tf.train.Saver()
+
+    init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
+        restore_dir = tf.train.latest_checkpoint(train_dir)
+        if restore_dir:
+            saver.restore(sess, restore_dir)
+            print('restore succeed')
+        else:
+            sess.run(init)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        for step in range(100000 + 1):
+            start_time = time.time()
+            batch = mnist.train.next_batch(64)
+            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            duration = time.time() - start_time
+            if step % 100 == 0:
+                valid_batch = mnist.validation.next_batch(64)
+                train_accuracy = accuracy.eval(feed_dict={x: valid_batch[0], y_: valid_batch[1], keep_prob: 1.0})
+                print("step {}, training accuracy {}, {}sec".format(step, train_accuracy, duration))
+            if step % 2000 == 0:
+                saver.save(sess, train_dir, global_step=step)
+        test_batch = mnist.test.next_batch(5000)
+        # print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+        print("1st part test accuracy %g" % accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1], keep_prob: 1.0}))
+        test_batch = mnist.test.next_batch(5000)
+        print("2nd part test accuracy %g" % accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1], keep_prob: 1.0}))
+        coord.request_stop()
+        coord.join(threads)
+    return
+
+
+if __name__ == "__main__":
+    tf.app.run()
